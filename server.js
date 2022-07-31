f21b4bc8f7435cb5792d64b78eebb971d060172c
RenatoLPSjr <renato.lop.jr@gmail.com>
2022-07-30 22:03:06 -0300

    Adicionando novas funções

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-const http = require("http")
-const port = 3001;
+import app from './src/app.js'
+const port = process.env.PORT || 3000;
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na pag de Livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Pag da editora'
-}
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-})
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
